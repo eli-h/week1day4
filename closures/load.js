18a110a88a11cffe1431e91c95c9ea8bd1db374a
eli-h <eli.heineman@gmail.com>
2017-07-27 21:24:15 +0000

    finished 1st closure

---
+++ closures/load.js
@@ -0,0 +1,22 @@
+// function makeLoadedDie() {
+// 	return function(random) {
+// 		return
+// 	}
+// }
+
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  var counter = -1
+  return function() {
+    /* your code here */
+    counter += 1
+    return list[counter];
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
